093e3f8c4e2d6c4592b2912bf1551da2f6e8590e
Start Coding Student 2 <session2@START-Coding-MBP-08.local>
2016-01-29 18:05:03 -0700

    Added bootstrap, made basic structure of website. Made tables for instruments.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php
 try {
-    $dbh = new PDO('mysql:host=127.0.0.1;dbname=mydb', 'root', 'root');
+    //$dbh = new PDO('mysql:host=127.0.0.1;dbname=mydb', 'root', 'root');
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
@@ -39,7 +39,7 @@ if(@$_POST['formSubmit'] == "Submit")
     {
         $errorMessage = "<li>You forgot to enter your security.</li>";
     }
-    $stmt = $dbh->prepare("INSERT INTO contact (email, name_first, password, card_name, card_number, card_expdate, card_security)
+    $stmt = $dbh->prepare("INSERT INTO users (email, name_first, password, card_name, card_number, card_expdate, card_security)
       VALUES (:email, :name_first, :password, :card_name, :card_number, :card_expdate, :card_security)");
     $result = $stmt->execute(array
     (
@@ -60,5 +60,52 @@ if(@$_POST['formSubmit'] == "Submit")
         echo("<ul>" . $errorMessage . "</ul>\n");
     }
 }?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Instruments Online</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div id="header">
+    <ul class="nav nav-tabs">
+        <li role="presentation" class="active"><a href="#">Home</a></li>
+        <li role="presentation"><a href="#"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+            </a></li>
+        <li role="presentation"><a href="#">Contact Us</a></li>
+    </ul>
+</div>
+
+<div id="content">
+    <table id="products" border="100px">
+    <tr>
+        <td><img src="https://s-media-cache-ak0.pinimg.com/236x/23/9a/64/239a640191cdf50f386e54f4cab38f0d.jpg" width="250px" height="300px"></td>
+        <td><img src="http://www.ozwinds.com.au/images/YRS-24B-700.jpg" width="250px" height="300px"></td>
+        <td><img src="http://westmusic.cachefly.net/getDynamicImage.aspx?w=800&h=800&b=00ffffff&path=Studio-49-1600-Series-AX-1600-Rosewood-Alto-Diatonic-Xylophone.jpg" width="250px" height="300px"></td>
+    </tr>
+        <tr>
+            <td><img src="http://drumshopglasgowonline.co.uk/images/CP221DWBongos.jpg" width="250px" height="300px"></td>
+            <td><img src="http://www.raleighmusiclessons.com/wp-content/uploads/2013/06/DV016_Jpg_Large_H73551_violin_and_bow.jpg" width="250px" height="300px"></td>
+            <td><img src="http://maton.com.au/assets/images/acoustic_product_MINID_2.jpg" width="250px" height="300px"></td>
+        </tr>
+        <tr>
+            <td><img src="" width="250px" height="300px"></td>
+            <td><img src="" width="250px" height="300px"></td>
+            <td><img src="" width="250px" height="300px"></td>
+        </tr>
+        <tr><td>piano</td></tr>
+    </table>
+</div>
+<div id="footer"><p>foot</p></div>
+
+
+</body>
+</html>
+
 
 
